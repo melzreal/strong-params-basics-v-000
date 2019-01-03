f49ae484ac0b87c2ea9ebe81e6ea1d8a85c257f9
Melissa Real <melissa.real@hotmail.com>
2019-01-03 17:08:08 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,19 +11,27 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+
+	def edit
+	  @post = Post.find(params[:id])
+	end
+
+
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
-	def edit
-	  @post = Post.find(params[:id])
+private
+
+	def post_params
+	  params.require(:post).permit(:title, :description)
 	end
 end
